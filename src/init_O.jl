dc820ec4ca9713b550854cc757e205b07f9514d4
jakeoung <jakeoung@gmail.com>
2021-02-22 01:15:06 +0100

    minor

--- src/init_O.jl
+++ src/init_O.jl
@@ -8,16 +8,11 @@ function init_O(I, H_high, W_high, mode="wfp")
         o_im = imresize(o_im, H_high, W_high)
         O0 = fftshift(fft(o_im))
     elseif mode == "gs"
-        ## Upsample the central low-resolution image
         A = imresize(I[:,:,trunc(Int,L/2)+1], H_high, W_high)
-
         Mag_image = H_high / H_low
-        ## Rescale to image for energy conservation
         scale = Mag_image.^2
         A = A./scale
         Hi_res_M, Hi_res_N = size(A)
-
-        ## Initialize HR image use the amplitude of central low-resolution image
         F = fftshift(fft(A))
     end
 end
